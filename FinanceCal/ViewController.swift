fbcfc15170238cdbb3b8bfb35e0b7529c5cf7ece
Rehanga Gamage <rehanga@live.com>
2023-07-22 09:57:55 +0530

    Initial Commit

---
+++ FinanceCal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceCal
+//
+//  Created by Rehanga Gamage on 2023-07-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
